2700e59c9158951e4184557fad46827f62a15000
owillis <owillis@iol.unh.edu>
2023-03-21 09:24:04 -0400

    Setup changes

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ A C++ implementation of Chinese checkers
 [Edge case for no possible moves](https://alexaanswers.amazon.com/question/4zVqU4K6yNAPbrBKjkc7A0#:~:text=If%20you%20are%20unable%20to,the%20game%20is%20a%20draw.)
 
 [Paper on chinese checkers](http://cs229.stanford.edu/proj2016spr/report/003.pdf)
+
+[Strongly Solving Chinese Checkers](https://webdocs.cs.ualberta.ca/~nathanst/papers/sturtevant2019chinesecheckers.pdf)
+
+[6.1 Chinese Checkers - Max^n play performs poor](https://webdocs.cs.ualberta.ca/~nathanst/papers/comparison_algorithms.pdf)
---
+++ finalProject/run.py
@@ -0,0 +1,47 @@
+import sys
+import os
+
+def printHelp():
+    print("usage: run.py [-h] ... [-c cmd | -m mod | file | -] [arg] ...\n"
+          "Options and arguments (and corresponding environment variables):")
+
+    print("optional arguments:")
+    print("  -h, --help\tshow this help message and exit")
+    exit(0)
+
+def printBoard(board):
+    for r in range(0, len(board)):
+        line = board[r]
+        linetab = r-1
+        if (r <= 5):
+            linetab = 9 - r
+        for t in range(0, linetab):
+            print("  ", end="")
+        for c in range(0, len(line)):
+            isEnd = c == len(line)-1
+            cur = line[c]
+            if cur != "-" and cur != " ":
+                print(f"{cur}", end=" ")
+            elif cur == "-":
+                print(" - ", end=" ")
+            if isEnd or cur == " ":
+                print()
+                break
+
+if __name__ == "__main__":
+    init = [["1_1", " ", " ", " ", " ", " "],
+            ["1_2", "1_3", " ", " ", " ", " "],
+            ["1_4", "1_5", "1_6", " ", " ", " "],
+            ["-", "-", "-", "-", " ", " "],
+            ["-", "-", "-", "-", "-", " "],
+            ["-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", " "],
+            ["-", "-", "-", "-", " ", " "],
+            ["2_4", "2_5", "2_6", " ", " ", " "],
+            ["2_2", "2_3", " ", " ", " ", " "],
+            ["2_1", " ", " ", " ", " ", " "]]
+    print("\n")
+    printBoard(init)
+    # print(f"Arguments count: {len(sys.argv)}")
+    # if '-help' in sys.argv or '-h' in sys.argv or '--help' in sys.argv:
+    #     printHelp()
